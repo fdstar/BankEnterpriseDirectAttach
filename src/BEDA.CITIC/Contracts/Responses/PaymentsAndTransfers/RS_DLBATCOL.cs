72b80fa3791ade96cf4fa6f2a9ca752b83d7b532
fangdong <fangdong@yodoo.net.cn>
2019-06-05 10:10:28 +0800

    中信银行（集团客户）  3.2.5 ~ 3.2.6

---
+++ src/BEDA.CITIC/Contracts/Responses/PaymentsAndTransfers/RS_DLBATCOL.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 批量支付汇总查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLBATCOL : RsBase
+    {
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlElement("list")]
+        public CITICCollection<PayBatchSumQry> Collection { get; set; }
+    }
+    /// <summary>
+    /// 批量支付汇总查询
+    /// </summary>
+    public class PayBatchSumQry
+    {
+        /// <summary>
+        /// 客户流水号char(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 交易日期char(8) 格式YYYYMMDD
+        /// </summary>
+        [XmlIgnore]
+        public DateTime PayDate { get; set; }
+        /// <summary>
+        /// 交易日期char(8) 格式YYYYMMDD, 对应<see cref="PayDate"/>
+        /// </summary>
+        [XmlElement("payDate")]
+        public string PayDateStr
+        {
+            get
+            {
+                return this.PayDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.PayDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 预约支付标志 0：非预约交易；1：预约交易 char(1)
+        /// </summary>
+        [XmlElement("preFlg")]
+        public int PreFlg { get; set; }
+        /// <summary>
+        /// 预约日期char(8) 格式YYYYMMDD
+        /// </summary>
+        [XmlElement("preDate")]
+        public string PreDate { get; set; }
+        /// <summary>
+        /// 预约时间char(6) 格式hhmmss
+        /// </summary>
+        [XmlElement("preTime")]
+        public string PreTime { get; set; }
+        /// <summary>
+        /// 预约时间 由<see cref="PreDate"/>和<see cref="PreTime"/>组成
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? DeferredPaymentTime
+        {
+            get
+            {
+                if (DateTime.TryParseExact(string.Format("{0}{1}", this.PreDate, this.PreTime),
+                   new string[] { "yyyyMMdd", "yyyyMMddHHmmss" }, CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    return tmp;
+                }
+                return null;
+            }
+        }
+        /// <summary>
+        /// 总笔数 int
+        /// </summary>
+        [XmlElement("totalNumber")]
+        public int TotalNumber { get; set; }
+        /// <summary>
+        /// 总金额decimal(15,2)
+        /// </summary>
+        [XmlElement("totalAmount")]
+        public decimal TotalAmount { get; set; }
+        /// <summary>
+        /// 摘要varchar(102)
+        /// </summary>
+        [XmlElement("abstract")]
+        public string Abstract { get; set; }
+        /// <summary>
+        /// 备注varchar(60)
+        /// </summary>
+        [XmlElement("memo")]
+        public string Memo { get; set; }
+        /// <summary>
+        /// 交易状态 char(7) ，值域参见见附录4.8
+        /// </summary>
+        [XmlElement("stt")]
+        public string Stt { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/PaymentsAndTransfers/RS_DLBATDET.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 批量支付明细查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLBATDET : RsBase
+    {
+        /// <summary>
+        /// 客户流水号char(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlElement("list")]
+        public CITICCollection<PayBatchDetailQry> Collection { get; set; }
+    }
+    /// <summary>
+    /// 批量支付明细查询
+    /// </summary>
+    public class PayBatchDetailQry
+    {
+        /// <summary>
+        /// 付款账号 char(19)
+        /// </summary>
+        [XmlElement("payAccountNo")]
+        public string PayAccountNo { get; set; }
+        /// <summary>
+        /// 收款账号varchar(32)
+        /// </summary>
+        [XmlElement("recAccountNo")]
+        public string RecAccountNo { get; set; }
+        /// <summary>
+        /// 收款账户名称 varchar(122)
+        /// </summary>
+        [XmlElement("recAccountName")]
+        public string RecAccountName { get; set; }
+        /// <summary>
+        /// 收款账户开户行联行网点号 varchar(20)
+        /// </summary>
+        [XmlElement("recOpenBankCode")]
+        public string RecOpenBankCode { get; set; }
+        /// <summary>
+        /// 收款账户开户行名称varchar(122)
+        /// </summary>
+        [XmlElement("recOpenBankName")]
+        public string RecOpenBankName { get; set; }
+        /// <summary>
+        /// 支付方式varchar(2)，02：大额支付；03：小额支付
+        /// </summary>
+        [XmlElement("payType")]
+        public string PayType { get; set; }
+        /// <summary>
+        /// 金额 decimal(15,2)
+        /// </summary>
+        [XmlElement("tranAmount")]
+        public decimal TranAmount { get; set; }
+        /// <summary>
+        /// 摘要 varchar(102)
+        /// </summary>
+        [XmlElement("abstract")]
+        public string Abstract { get; set; }
+        /// <summary>
+        /// 备注 varchar(60)
+        /// </summary>
+        [XmlElement("memo")]
+        public string Memo { get; set; }
+        /// <summary>
+        /// 状态代码char(7) ，值域参见见附录4.8
+        /// </summary>
+        [XmlElement("status")]
+        public string Status { get; set; }
+        /// <summary>
+        /// 状态信息含义varchar(254)
+        /// </summary>
+        [XmlElement("statusText")]
+        public string StatusText { get; set; }
+    }
+}
