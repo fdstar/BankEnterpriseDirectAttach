2e7e702880fa51a4a34468cd68ce2dbedbedaf49
fangdong <fangdong@yodoo.net.cn>
2019-06-19 16:12:13 +0800

    中信银行（集团客户） 流动性管理  3.4.28 ~ 3.4.29

---
+++ src/BEDA.CITIC/Contracts/Requests/LiquidityManagement/RQ_DLPPDSUB.cs
@@ -0,0 +1,134 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 成员单位利息分配经办请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLPPDSUB : RqBase<RS_DLPPDSUB>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLPPDSUB"; set { } }
+        /// <summary>
+        /// 客户流水号 varchar(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 成员单位账号 char(19)
+        /// </summary>
+        [XmlElement("accountNo")]
+        public string AccountNo { get; set; }
+        /// <summary>
+        /// 现金池ID  char(8) 由现金池信息查询结果获取
+        /// </summary>
+        [XmlElement("poolID")]
+        public string PoolID { get; set; }
+        /// <summary>
+        /// 起息日期 char(8) 格式YYYYMMDD，由成员单位利息分配经办查询返回
+        /// </summary>
+        [XmlIgnore]
+        public DateTime RateDate { get; set; }
+        /// <summary>
+        /// 起息日期 char(8) 格式YYYYMMDD, 对应<see cref="RateDate"/>
+        /// </summary>
+        [XmlElement("rateDate")]
+        public string RateDateStr
+        {
+            get
+            {
+                return this.RateDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.RateDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 结息日期 char(8) 格式YYYYMMDD，由成员单位利息分配经办查询返回
+        /// </summary>
+        [XmlIgnore]
+        public DateTime DueDate { get; set; }
+        /// <summary>
+        /// 结息日期 char(8) 格式YYYYMMDD, 对应<see cref="DueDate"/>
+        /// </summary>
+        [XmlElement("dueDate")]
+        public string DueDateStr
+        {
+            get
+            {
+                return this.DueDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.DueDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 应付利息 decimal(15,2) ，由成员单位利息分配经办查询返回
+        /// </summary>
+        [XmlElement("payAmt")]
+        public decimal PayAmt { get; set; }
+        /// <summary>
+        /// 应收利息 decimal(15,2)，由成员单位利息分配经办查询返回
+        /// </summary>
+        [XmlElement("rcvAmt")]
+        public decimal RcvAmt { get; set; }
+        /// <summary>
+        /// 预约支付标志 0：非预约交易；1：预约交易 char(1)
+        /// </summary>
+        [XmlElement("preFlg")]
+        public int PreFlg { get; set; }
+        /// <summary>
+        /// 延期支付时间
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? AppointmentTime { get; set; }
+        /// <summary>
+        /// 延期支付日期char(8)，预约支付时非空，格式YYYYMMDD
+        /// </summary>
+        [XmlElement("preDate")]
+        public string PreDateStr
+        {
+            get
+            {
+                return this.AppointmentTime?.ToString("yyyyMMdd");
+            }
+            set { }
+        }
+        /// <summary>
+        /// 延期支付时间char(6)，预约支付时非空，格式hhmmss
+        /// </summary>
+        [XmlElement("preTime")]
+        public string PreTimeStr
+        {
+            get
+            {
+                return this.AppointmentTime?.ToString("HHmmss");
+            }
+            set { }
+        }
+        /// <summary>
+        /// 备注 varchar(60) 可空
+        /// </summary>
+        [XmlElement("memo")]
+        public string Memo { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/LiquidityManagement/RS_DLPRDQRY.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 成员单位利息分配经办结果查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLPRDQRY : RsBase
+    {
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<UnitInterestResult> List { get; set; }
+    }
+}
