5e5ab9e5f9f15622a1b0442d26df8dbaf4d21e08
fangdong <fangdong@yodoo.net.cn>
2019-06-21 13:40:16 +0800

    中信银行（集团客户）附属账户  3.5.9 ~ 3.5.10

---
+++ src/BEDA.CITIC/Contracts/Requests/SubAccount/RQ_DLSJXSUB.cs
@@ -0,0 +1,85 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 附属账户利息计算经办请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLSJXSUB : RqBase<RS_DLSJXSUB>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLSJXSUB"; set { } }
+        /// <summary>
+        /// 客户流水号 varchar(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 附属账号 char(19)
+        /// </summary>
+        [XmlElement("subAccNo")]
+        public string SubAccNo { get; set; }
+        /// <summary>
+        /// 起息日期 char(8) 格式YYYYMMDD，由附属账户利息计算经办查询返回
+        /// </summary>
+        [XmlIgnore]
+        public DateTime RateDate { get; set; }
+        /// <summary>
+        /// 起息日期 char(8) 格式YYYYMMDD, 对应<see cref="RateDate"/>
+        /// </summary>
+        [XmlElement("rateDate")]
+        public string RateDateStr
+        {
+            get
+            {
+                return this.RateDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.RateDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 结息日期 char(8) 格式YYYYMMDD，由附属账户利息计算经办查询返回
+        /// </summary>
+        [XmlIgnore]
+        public DateTime DueDate { get; set; }
+        /// <summary>
+        /// 结息日期 char(8) 格式YYYYMMDD, 对应<see cref="DueDate"/>
+        /// </summary>
+        [XmlElement("dueDate")]
+        public string DueDateStr
+        {
+            get
+            {
+                return this.DueDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.DueDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 应付利息 decimal(15,2) ，由附属账户利息计算经办查询返回
+        /// </summary>
+        [XmlElement("payAmt")]
+        public decimal PayAmt { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/SubAccount/RS_DLSJXQRY.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 附属账户利息计算经办结果查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLSJXQRY : RsBase
+    {
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<SubAccountInterestResult> List { get; set; }
+    }
+    /// <summary>
+    /// 附属账户利息计算经办结果
+    /// </summary>
+    public class SubAccountInterestResult
+    {
+        /// <summary>
+        /// 批次号 varchar(10)
+        /// </summary>
+        [XmlElement("batNo")]
+        public string BatNo { get; set; }
+        /// <summary>
+        /// 附属账号 char(19)
+        /// </summary>
+        [XmlElement("subAccNo")]
+        public string SubAccNo { get; set; }
+        /// <summary>
+        /// 附属账户名称 varchar(122)
+        /// </summary>
+        [XmlElement("subAccNm")]
+        public string SubAccNm { get; set; }
+        /// <summary>
+        /// 利息 decimal(15,2)
+        /// </summary>
+        [XmlElement("payAmt")]
+        public decimal PayAmt { get; set; }
+        /// <summary>
+        /// 起息日期 char(8) 格式YYYYMMDD
+        /// </summary>
+        [XmlIgnore]
+        public DateTime RateDate { get; set; }
+        /// <summary>
+        /// 起息日期 char(8) 格式YYYYMMDD, 对应<see cref="RateDate"/>
+        /// </summary>
+        [XmlElement("rateDate")]
+        public string RateDateStr
+        {
+            get
+            {
+                return this.RateDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.RateDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 结息日期 char(8) 格式YYYYMMDD
+        /// </summary>
+        [XmlIgnore]
+        public DateTime DueDate { get; set; }
+        /// <summary>
+        /// 结息日期 char(8) 格式YYYYMMDD, 对应<see cref="DueDate"/>
+        /// </summary>
+        [XmlElement("dueDate")]
+        public string DueDateStr
+        {
+            get
+            {
+                return this.DueDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.DueDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 币种 char(2)
+        /// </summary>
+        [XmlElement("cryType")]
+        public string CryType { get; set; }
+        /// <summary>
+        /// 经办人姓名 varchar(62)
+        /// </summary>
+        [XmlElement("operatorNm")]
+        public string OperatorNm { get; set; }
+        /// <summary>
+        /// 经办日期 char(8) YYYYMMDD
+        /// </summary>
+        [XmlElement("operateDate")]
+        public string OperateDate { get; set; }
+        /// <summary>
+        /// 经办时间 char(6) 格式hhmmss
+        /// </summary>
+        [XmlElement("operateTime")]
+        public string OperateTime { get; set; }
+        /// <summary>
+        /// 经办时间 由<see cref="OperateDate"/>和<see cref="OperateTime"/>组成
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? OperatingTime
+        {
+            get
+            {
+                if (DateTime.TryParseExact(string.Format("{0}{1}", this.OperateDate, this.OperateTime),
+                   new string[] { "yyyyMMdd", "yyyyMMddHHmmss" }, CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    return tmp;
+                }
+                return null;
+            }
+        }
+        /// <summary>
+        /// 制单状态 varchar(2) 状态取值见附录4.2
+        /// </summary>
+        [XmlElement("Stt")]
+        public string Stt { get; set; }
+    }
+}
