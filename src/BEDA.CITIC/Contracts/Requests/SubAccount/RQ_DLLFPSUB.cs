10cb06e31aa01c16913e0e1df1144e244cd8bf36
fangdong <fangdong@yodoo.net.cn>
2019-06-24 19:31:39 +0800

    中信银行（集团客户）附属账户  3.5.11 ~ 3.5.12

---
+++ src/BEDA.CITIC/Contracts/Requests/SubAccount/RQ_DLLFPSUB.cs
@@ -0,0 +1,119 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 附属账户利息分配经办请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLLFPSUB : RqBase<RS_DLLFPSUB>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLLFPSUB"; set { } }
+        /// <summary>
+        /// 客户流水号 varchar(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 附属账号 char(19)
+        /// </summary>
+        [XmlElement("subAccNo")]
+        public string SubAccNo { get; set; }
+        /// <summary>
+        /// 起息日期 char(8) 格式YYYYMMDD，由附属账户利息分配经办查询返回
+        /// </summary>
+        [XmlIgnore]
+        public DateTime RateDate { get; set; }
+        /// <summary>
+        /// 起息日期 char(8) 格式YYYYMMDD, 对应<see cref="RateDate"/>
+        /// </summary>
+        [XmlElement("rateDate")]
+        public string RateDateStr
+        {
+            get
+            {
+                return this.RateDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.RateDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 结息日期 char(8) 格式YYYYMMDD，由附属账户利息分配经办查询返回
+        /// </summary>
+        [XmlIgnore]
+        public DateTime DueDate { get; set; }
+        /// <summary>
+        /// 结息日期 char(8) 格式YYYYMMDD, 对应<see cref="DueDate"/>
+        /// </summary>
+        [XmlElement("dueDate")]
+        public string DueDateStr
+        {
+            get
+            {
+                return this.DueDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.DueDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 应付利息 decimal(15,2) ，由附属账户利息分配经办查询返回
+        /// </summary>
+        [XmlElement("payAmt")]
+        public decimal PayAmt { get; set; }
+        /// <summary>
+        /// 预约标志 0：非预约1：预约 char(1)
+        /// </summary>
+        [XmlElement("preFlg")]
+        public int PreFlg { get; set; }
+        /// <summary>
+        /// 预约时间，预约时非空，时间点可选范围 100000，120000，140000，160000
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? AppointmentTime { get; set; }
+        /// <summary>
+        /// 预约日期 char(8) 格式 YYYYMMDD 预约时非空
+        /// </summary>
+        [XmlElement("preDate")]
+        public string PreDateStr
+        {
+            get
+            {
+                return this.AppointmentTime?.ToString("yyyyMMdd");
+            }
+            set { }
+        }
+        /// <summary>
+        /// 预约时间 char(6) 格式 hhmmss 预约时非空 只限100000，120000，140000，160000四个时间点
+        /// </summary>
+        [XmlElement("preTime")]
+        public string PreTimeStr
+        {
+            get
+            {
+                return this.AppointmentTime?.ToString("HHmmss");
+            }
+            set { }
+        }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/SubAccount/RS_DLITSQRY.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 附属账户利息分配经办查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLITSQRY : RsBase
+    {
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<SubAccountInterestShare> List { get; set; }
+    }
+    /// <summary>
+    /// 附属账户利息分配经办
+    /// </summary>
+    public class SubAccountInterestShare
+    {
+        /// <summary>
+        /// 附属账号 char(19)
+        /// </summary>
+        [XmlElement("subAccNo")]
+        public string SubAccNo { get; set; }
+        /// <summary>
+        /// 附属账户名称 varchar(122)
+        /// </summary>
+        [XmlElement("subAccNm")]
+        public string SubAccNm { get; set; }
+        /// <summary>
+        /// 应付利息 decimal(15,2)
+        /// </summary>
+        [XmlElement("payAmt")]
+        public decimal PayAmt { get; set; }
+        /// <summary>
+        /// 起息日期 char(8) 格式YYYYMMDD
+        /// </summary>
+        [XmlIgnore]
+        public DateTime RateDate { get; set; }
+        /// <summary>
+        /// 起息日期 char(8) 格式YYYYMMDD, 对应<see cref="RateDate"/>
+        /// </summary>
+        [XmlElement("rateDate")]
+        public string RateDateStr
+        {
+            get
+            {
+                return this.RateDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.RateDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 结息日期 char(8) 格式YYYYMMDD
+        /// </summary>
+        [XmlIgnore]
+        public DateTime DueDate { get; set; }
+        /// <summary>
+        /// 结息日期 char(8) 格式YYYYMMDD, 对应<see cref="DueDate"/>
+        /// </summary>
+        [XmlElement("dueDate")]
+        public string DueDateStr
+        {
+            get
+            {
+                return this.DueDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.DueDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 币种 char(2)
+        /// </summary>
+        [XmlElement("cryType")]
+        public string CryType { get; set; }
+    }
+}
