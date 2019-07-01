ae122db88b2e1107ecda41e1a19439dddb3ab14c
fangdong <fangdong@yodoo.net.cn>
2019-07-01 20:05:48 +0800

    中信银行（集团客户）投资理财 3.6.4 ~ 3.6.5

---
+++ src/BEDA.CITIC/Contracts/Requests/FinanceAndInvestment/RQ_DLNTOPEN.cs
@@ -0,0 +1,49 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 定期转活期经办请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLNTOPEN : RqBase<RS_DLNTOPEN>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLNTOPEN"; set { } }
+        /// <summary>
+        /// 客户流水号 varchar(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 活期账号 char(19)
+        /// </summary>
+        [XmlElement("accountNo")]
+        public string AccountNo { get; set; }
+        /// <summary>
+        /// 交易金额 decimal（15，2）
+        /// </summary>
+        [XmlElement("tranAmt")]
+        public decimal TranAmt { get; set; }
+        /// <summary>
+        /// 通知种类 char(2) 01：一天通知；02：七天通知
+        /// </summary>
+        [XmlElement("notifyType")]
+        public string NotifyType { get; set; }
+        /// <summary>
+        /// 备注 varchar(60)
+        /// </summary>
+        [XmlElement("memo")]
+        public string Memo { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/FinanceAndInvestment/RS_DLNTOPEN.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 定期转活期经办响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLNTOPEN : RsBase
+    {
+    }
+}
