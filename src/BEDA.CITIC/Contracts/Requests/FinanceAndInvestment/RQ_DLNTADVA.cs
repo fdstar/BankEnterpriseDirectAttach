1436f52788b7f1035db042130b9dadf5249cc46d
fangdong <fangdong@yodoo.net.cn>
2019-07-02 19:51:54 +0800

    中信银行（集团客户）投资理财 3.6.6 ~ 3.6.7

---
+++ src/BEDA.CITIC/Contracts/Requests/FinanceAndInvestment/RQ_DLNTADVA.cs
@@ -0,0 +1,54 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 通知存款提前支取经办请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLNTADVA : RqBase<RS_DLNTADVA>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLNTADVA"; set { } }
+        /// <summary>
+        /// 客户流水号 varchar(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 活期账号 char(19)
+        /// </summary>
+        [XmlElement("accountNo")]
+        public string AccountNo { get; set; }
+        /// <summary>
+        /// 通知账号 char(19) 由通知存款查询返回
+        /// </summary>
+        [XmlElement("notifyAccNo")]
+        public string NotifyAccNo { get; set; }
+        /// <summary>
+        /// 支取方式 char(1) 0：普通支取；1：结清
+        /// </summary>
+        [XmlElement("drawType")]
+        public int DrawType { get; set; }
+        /// <summary>
+        /// 交易金额 decimal(15，2)
+        /// </summary>
+        [XmlElement("tranAmt")]
+        public decimal TranAmt { get; set; }
+        /// <summary>
+        /// 经办备注 varchar(60)，可空
+        /// </summary>
+        [XmlElement("memo")]
+        public string Memo { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Requests/FinanceAndInvestment/RQ_DLNTSTOP.cs
@@ -0,0 +1,44 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 通知取消经办请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLNTSTOP : RqBase<RS_DLNTSTOP>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLNTSTOP"; set { } }
+        /// <summary>
+        /// 客户流水号 varchar(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 活期账号 char(19)
+        /// </summary>
+        [XmlElement("accountNo")]
+        public string AccountNo { get; set; }
+        /// <summary>
+        /// 通知账号 char(19)，由通知取消经办前查询返回
+        /// </summary>
+        [XmlElement("notifyAccNo")]
+        public string NotifyAccNo { get; set; }
+        /// <summary>
+        /// 备注 varchar(60)，可空
+        /// </summary>
+        [XmlElement("memo")]
+        public string Memo { get; set; }
+    }
+}
