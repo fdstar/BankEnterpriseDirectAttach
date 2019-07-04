d30b56002e77befd8c6f34514394d13daf00524f
fangdong <fangdong@yodoo.net.cn>
2019-07-04 11:43:49 +0800

    中信银行（集团客户）第三方存管 3.7.3 ~ 3.7.4

---
+++ src/BEDA.CITIC/Contracts/Requests/ThirdPartyDepository/RQ_DLBKADET.cs
@@ -0,0 +1,82 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 银证转账明细查询请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLBKADET : RqBase<RS_DLBKADET>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLBKADET"; set { } }
+        /// <summary>
+        /// 他行账号 varchar(32)
+        /// </summary>
+        [XmlElement("accountNo")]
+        public string AccountNo { get; set; }
+        /// <summary>
+        /// 证券公司代码 char(8) ，由台账账号信息查询返回
+        /// </summary>
+        [XmlElement("agentCode")]
+        public string AgentCode { get; set; }
+        /// <summary>
+        /// 起始日期 char(8) 格式:YYYYMMDD 可空，为空时默认为当天
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? StartDate { get; set; }
+        /// <summary>
+        /// 起始日期char(8) 格式YYYYMMDD, 对应<see cref="StartDate"/>
+        /// </summary>
+        [XmlElement("startDate")]
+        public string StartDateStr
+        {
+            get
+            {
+                return this.StartDate?.ToString("yyyyMMdd");
+            }
+            set
+            {
+                this.StartDate = null;
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.StartDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 截止日期 char(8) 格式:YYYYMMDD 可空，为空时默认为当天
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? EndDate { get; set; }
+        /// <summary>
+        /// 终止日期char(8) 格式YYYYMMDD, 对应<see cref="EndDate"/>
+        /// </summary>
+        [XmlElement("endDate")]
+        public string EndDateStr
+        {
+            get
+            {
+                return this.EndDate?.ToString("yyyyMMdd");
+            }
+            set
+            {
+                this.EndDate = null;
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.EndDate = tmp;
+                }
+            }
+        }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/ThirdPartyDepository/RS_DLBKADET.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 银证转账明细查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLBKADET : RsBase
+    {
+        /// <summary>
+        /// 总笔数 int
+        /// </summary>
+        [XmlElement("totalNum")]
+        public int TotalNum { get; set; }
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<AgentTransfer> List { get; set; }
+    }
+    /// <summary>
+    /// 银证转账
+    /// </summary>
+    public class AgentTransfer
+    {
+        /// <summary>
+        /// 交易日期 char(8) 格式YYYYMMDD
+        /// </summary>
+        [XmlElement("tranDate")]
+        public string TranDate { get; set; }
+        /// <summary>
+        /// 交易时间 char(6) 格式hhmmss
+        /// </summary>
+        [XmlElement("tranTime")]
+        public string TranTime { get; set; }
+        /// <summary>
+        /// 交易时间 由<see cref="TranDate"/>和<see cref="TranTime"/>组成
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? TransactionTime
+        {
+            get
+            {
+                if (DateTime.TryParseExact(string.Format("{0}{1}", this.TranDate, this.TranTime),
+                   new string[] { "yyyyMMdd", "yyyyMMddHHmmss" }, CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    return tmp;
+                }
+                return null;
+            }
+        }
+        /// <summary>
+        /// 存管系统流水号 char(12)
+        /// </summary>
+        [XmlElement("flowNo")]
+        public string FlowNo { get; set; }
+        /// <summary>
+        /// 交易金额 decimal(15,2)
+        /// </summary>
+        [XmlElement("tranAmt")]
+        public decimal TranAmt { get; set; }
+        /// <summary>
+        /// 转账类型 char(1) 0：银转证；1：证转银
+        /// </summary>
+        [XmlElement("tranType")]
+        public int TranType { get; set; }
+        /// <summary>
+        /// 转账结果 char(1)0：成功；1：失败；2：已冲正
+        /// </summary>
+        [XmlElement("tranResult")]
+        public int TranResult { get; set; }
+        /// <summary>
+        /// 发起方渠道 char(1) 格式见附录4.4
+        /// </summary>
+        [XmlElement("channel")]
+        public string Channel { get; set; }
+        /// <summary>
+        /// 币种 char(2)
+        /// </summary>
+        [XmlElement("cryType")]
+        public string CryType { get; set; }
+    }
+}
