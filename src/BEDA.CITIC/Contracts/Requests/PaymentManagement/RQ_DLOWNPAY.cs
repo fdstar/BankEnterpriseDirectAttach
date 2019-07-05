3051ea26d3a05bd2dc470c8d37802284b9ad7534
fangdong <fangdong@yodoo.net.cn>
2019-07-05 13:41:42 +0800

    中信银行（集团客户）付款管理 3.8.1 ~ 3.8.2

---
+++ src/BEDA.CITIC/Contracts/Requests/PaymentManagement/RQ_DLOWNPAY.cs
@@ -0,0 +1,49 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 应付账单请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLOWNPAY : RqBase<RS_DLOWNPAY>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLOWNPAY"; set { } }
+        /// <summary>
+        /// 客户流水号 varchar(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 签约账号 char(19)
+        /// </summary>
+        [XmlElement("accountNo")]
+        public string AccountNo { get; set; }
+        /// <summary>
+        /// 经办标志 char(1) 0：同意；1：拒绝
+        /// </summary>
+        [XmlElement("tranFlg")]
+        public int TranFlg { get; set; }
+        /// <summary>
+        /// 拒绝原因 varchar(200) 经办标志为1（拒绝）时非空
+        /// </summary>
+        [XmlElement("refRem")]
+        public string RefRem { get; set; }
+        /// <summary>
+        /// 账单流水号char(20) ，由应付账单经办查询返回
+        /// </summary>
+        [XmlElement("payOrderNo")]
+        public string PayOrderNo { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/PaymentManagement/RS_DLOWPYQY.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 应付账单经办查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLOWPYQY : RsBase
+    {
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<PayableBill> List { get; set; }
+    }
+    /// <summary>
+    /// 应付账单经办
+    /// </summary>
+    public class PayableBill
+    {
+        /// <summary>
+        /// 批次号 varchar(8)
+        /// </summary>
+        [XmlElement("batNo")]
+        public string BatNo { get; set; }
+        /// <summary>
+        /// 账单流水号 char(20)
+        /// </summary>
+        [XmlElement("payOrderNo")]
+        public string PayOrderNo { get; set; }
+        /// <summary>
+        /// 付款账号 char(19)
+        /// </summary>
+        [XmlElement("payAccNo")]
+        public string PayAccNo { get; set; }
+        /// <summary>
+        /// 收款账号 char(19)
+        /// </summary>
+        [XmlElement("rcvAccNo")]
+        public string RcvAccNo { get; set; }
+        /// <summary>
+        /// 收款账户名称 varchar(122)
+        /// </summary>
+        [XmlElement("rcvAccNm")]
+        public string RcvAccNm { get; set; }
+        /// <summary>
+        /// 币种 char(2)
+        /// </summary>
+        [XmlElement("cryType")]
+        public string CryType { get; set; }
+        /// <summary>
+        /// 交易金额 decimal(15,2)
+        /// </summary>
+        [XmlElement("tranAmt")]
+        public decimal TranAmt { get; set; }
+        /// <summary>
+        /// 摘要 varchar(102)
+        /// </summary>
+        [XmlElement("abstract")]
+        public string Abstract { get; set; }
+    }
+}
