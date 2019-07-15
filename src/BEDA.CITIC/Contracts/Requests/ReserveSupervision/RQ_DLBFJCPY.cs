4bbdd60d62194137f485813e0a32f3d4dc0cb681
fangdong <fangdong@yodoo.net.cn>
2019-07-15 19:42:48 +0800

    中信银行（集团客户）备付金监管 3.13.5 ~ 3.13.6

---
+++ src/BEDA.CITIC/Contracts/Requests/ReserveSupervision/RQ_DLBFJCPY.cs
@@ -0,0 +1,83 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 备付金归集经办请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLBFJCPY : RqBase<RS_DLBFJCPY>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLBFJCPY"; set { } }
+        /// <summary>
+        /// 客户流水号 varchar(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 协议编号varchar(60)
+        /// </summary>
+        [XmlElement("ptcID")]
+        public string PtcID { get; set; }
+        /// <summary>
+        /// 交易金额 decimal(15,2)
+        /// </summary>
+        [XmlElement("tranAmount")]
+        public decimal TranAmount { get; set; }
+        /// <summary>
+        /// 预约代收标志 char(1)  0：非预约；1：预约
+        /// </summary>
+        [XmlElement("preFlg")]
+        public int PreFlg { get; set; }
+        /// <summary>
+        /// 延期支付时间，预约时非空
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? AppointmentTime { get; set; }
+        /// <summary>
+        /// 延期支付日期char(8)　格式YYYYMMDD ，预约时非空
+        /// </summary>
+        [XmlElement("preDate")]
+        public string PreDateStr
+        {
+            get
+            {
+                return this.AppointmentTime?.ToString("yyyyMMdd");
+            }
+            set { }
+        }
+        /// <summary>
+        /// 延期支付时间char(6)  格式hhmmss，预约时非空
+        /// </summary>
+        [XmlElement("preTime")]
+        public string PreTimeStr
+        {
+            get
+            {
+                return this.AppointmentTime?.ToString("HHmmss");
+            }
+            set { }
+        }
+        /// <summary>
+        /// 摘要 varchar(102)
+        /// </summary>
+        [XmlElement("abstract")]
+        public string Abstract { get; set; }
+        /// <summary>
+        /// 备注 varchar(60)，可空
+        /// </summary>
+        [XmlElement("memo")]
+        public string Memo { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/ReserveSupervision/RS_DLBFJCPY.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 备付金归集经办响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLBFJCPY : RsBase
+    {
+    }
+}
