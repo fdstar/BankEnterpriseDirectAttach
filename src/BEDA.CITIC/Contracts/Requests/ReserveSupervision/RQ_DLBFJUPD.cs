6ca85d2fa5182cfd866a50902dfa0c5292c8acff
fangdong <fangdong@yodoo.net.cn>
2019-07-16 09:51:36 +0800

    中信银行（集团客户）备付金监管 3.13.7 ~ 3.13.8

---
+++ src/BEDA.CITIC/Contracts/Requests/ReserveSupervision/RQ_DLBFJUPD.cs
@@ -0,0 +1,74 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 白名单经办(新增)请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLBFJUPD : RqBase<RS_DLBFJUPD>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLBFJUPD"; set { } }
+        /// <summary>
+        /// 客户流水号 varchar(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 本/他行标志char(1)，0：中信；1：他行
+        /// </summary>
+        [XmlElement("citicbankFlag")]
+        public int CiticbankFlag { get; set; }
+        /// <summary>
+        /// 币种char(2) ，01 ：人民币
+        /// </summary>
+        [XmlElement("cryType")]
+        public string CryType { get; set; }
+        /// <summary>
+        /// 账户类别char(1)，0：对公；1：个人
+        /// </summary>
+        [XmlElement("accType")]
+        public int AccType { get; set; }
+        /// <summary>
+        /// 收款人账号 char(19)
+        /// </summary>
+        [XmlElement("accNo")]
+        public string AccNo { get; set; }
+        /// <summary>
+        /// 收款人名称 varchar(122)
+        /// </summary>
+        [XmlElement("accNm")]
+        public string AccNm { get; set; }
+        /// <summary>
+        /// 收款账号开户行名称  varchar(122)
+        /// </summary>
+        [XmlElement("bankName")]
+        public string BankName { get; set; }
+        /// <summary>
+        /// 收款账号开户行 varchar(20)
+        /// </summary>
+        [XmlElement("bankCode")]
+        public string BankCode { get; set; }
+        /// <summary>
+        /// 是否强制更新 0：否 1：是
+        /// </summary>
+        [XmlElement("updFlag")]
+        public int UpdFlag { get; set; }
+        /// <summary>
+        /// 白名单类型varchar(1) 0:预付卡备付金支付 1:非预付卡备付金支付, 可空
+        /// </summary>
+        [XmlElement("wltype")]
+        public int? Wltype { get; set; }
+    }
+}
