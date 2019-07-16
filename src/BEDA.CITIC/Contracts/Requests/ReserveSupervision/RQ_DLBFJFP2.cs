9a281962c1c2bf5306702f154dadb422c9aa12d9
fangdong <fangdong@yodoo.net.cn>
2019-07-16 14:23:26 +0800

    中信银行（集团客户）备付金监管 3.13.13 ~ 3.13.14

---
+++ src/BEDA.CITIC/Contracts/Requests/ReserveSupervision/RQ_DLBFJFP2.cs
@@ -0,0 +1,92 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 非预付卡备付金其他代付经办请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLBFJFP2 : RqBase<RS_DLBFJFP2>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLBFJFP2"; set { } }
+        /// <summary>
+        /// 客户流水号char(20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 总笔数  必须为数字char(4)
+        /// </summary>
+        [XmlElement("totalNumber")]
+        public int TotalNumber { get { return this.Collection?.List?.Count ?? 0; } set { } }
+        /// <summary>
+        /// 总金额 deciam(15,2)
+        /// </summary>
+        [XmlElement("totalAmount")]
+        public decimal TotalAmount { get { return this.Collection?.List?.Sum(x => x.TranAmount) ?? 0; } set { } }
+        /// <summary>
+        /// 付款账号 char(19)
+        /// </summary>
+        [XmlElement("payAccountNo")]
+        public string PayAccountNo { get; set; }
+        /// <summary>
+        /// 预约支付标志 0：非预约交易；1：预约交易 char(1)
+        /// </summary>
+        [XmlElement("preFlg")]
+        public int PreFlg { get; set; }
+        /// <summary>
+        /// 延期支付时间，预约时非空
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? AppointmentTime { get; set; }
+        /// <summary>
+        /// 延期支付日期char(8)　格式YYYYMMDD
+        /// </summary>
+        [XmlElement("preDate")]
+        public string PreDateStr
+        {
+            get
+            {
+                return this.AppointmentTime?.ToString("yyyyMMdd");
+            }
+            set { }
+        }
+        /// <summary>
+        /// 延期支付时间char(6)  格式hhmmss
+        /// </summary>
+        [XmlElement("preTime")]
+        public string PreTimeStr
+        {
+            get
+            {
+                return this.AppointmentTime?.ToString("HHmmss");
+            }
+            set { }
+        }
+        /// <summary>
+        /// 备注varchar(60)，可空
+        /// </summary>
+        [XmlElement("memo")]
+        public string Memo { get; set; }
+        /// <summary>
+        /// 摘要，可空 varchar(102)
+        /// </summary>
+        [XmlElement("abstract")]
+        public string Abstract { get; set; }
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlElement("list")]
+        public CITICCollection<OtherPayment> Collection { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/ReserveSupervision/RS_DLBFJYP2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 预付卡备付金其他代付经办响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLBFJYP2 : RsBase
+    {
+        /// <summary>
+        /// 成功总笔数 int
+        /// </summary>
+        [XmlElement("sucTotalNum")]
+        public int SucTotalNum { get; set; }
+        /// <summary>
+        /// 成功总金额decimal(15,2)
+        /// </summary>
+        [XmlElement("sucTotalAmt")]
+        public decimal SucTotalAmt { get; set; }
+        /// <summary>
+        /// 失败总笔数 int
+        /// </summary>
+        [XmlElement("errTotalNum")]
+        public int ErrTotalNum { get; set; }
+        /// <summary>
+        /// 失败总金额decimal(15,2)
+        /// </summary>
+        [XmlElement("errTotalAmt")]
+        public decimal ErrTotalAmt { get; set; }
+
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<PayResult> List { get; set; }
+    }
+    /// <summary>
+    /// 备付金账户余额
+    /// </summary>
+    public class PayResult
+    {
+        /// <summary>
+        /// 交易状态 char(7)
+        /// </summary>
+        [XmlElement("status")]
+        public string Status { get; set; }
+        /// <summary>
+        /// 交易状态信息 varchar(254)
+        /// </summary>
+        [XmlElement("statusText")]
+        public string StatusText { get; set; }
+        /// <summary>
+        /// 收款账号varchar(19)
+        /// </summary>
+        [XmlElement("recAccountNo")]
+        public string RecAccountNo { get; set; }
+    }
+}
