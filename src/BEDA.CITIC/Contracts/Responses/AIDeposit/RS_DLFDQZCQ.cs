d215afcd629c63a6b3bae806bc48c680c0784dcb
fangdong <fangdong@yodoo.net.cn>
2019-07-18 14:50:23 +0800

    中信银行（集团客户）智能存款 3.14.11 ~ 3.14.12

---
+++ src/BEDA.CITIC/Contracts/Responses/AIDeposit/RS_DLFDQZCQ.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 组合型智能存款利息汇总查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLFDQZCQ : RsBase
+    {
+        /// <summary>
+        /// 账号char(19)
+        /// </summary>
+        [XmlElement("accountNo")]
+        public string AccountNo { get; set; }
+        /// <summary>
+        /// 账户名称char(122)
+        /// </summary>
+        [XmlElement("accountNm")]
+        public string AccountNm { get; set; }
+
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<MixedTypeAIDepositInterestSummary> List { get; set; }
+    }
+    /// <summary>
+    /// 组合型智能存款利息汇总
+    /// </summary>
+    public class MixedTypeAIDepositInterestSummary
+    {
+        /// <summary>
+        /// 利息金额decimal(15,2)
+        /// </summary>
+        [XmlElement("inrtAmt")]
+        public decimal InrtAmt { get; set; }
+        /// <summary>
+        /// 入息日期char(8)，格式YYYYMMDD
+        /// </summary>
+        [XmlIgnore]
+        public DateTime RcvDate { get; set; }
+        /// <summary>
+        /// 入息日期char(8)，格式YYYYMMDD, 对应<see cref="RcvDate"/>
+        /// </summary>
+        [XmlElement("rcvDate")]
+        public string RcvDateStr
+        {
+            get
+            {
+                return this.RcvDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.RcvDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 币种char(2)
+        /// </summary>
+        [XmlElement("cryType")]
+        public string CryType { get; set; }
+        /// <summary>
+        /// 活期利息decimal(15,2)
+        /// </summary>
+        [XmlElement("curInrt")]
+        public decimal CurInrt { get; set; }
+        /// <summary>
+        /// 定期利息decimal(15,2)
+        /// </summary>
+        [XmlElement("fixInrt")]
+        public decimal FixInrt { get; set; }
+        /// <summary>
+        /// 年份char(4)
+        /// </summary>
+        [XmlElement("xtYear")]
+        public int XtYear { get; set; }
+        /// <summary>
+        /// 季度 char(1) 1：第一季度；2：第二季度；3：第三季度；4：第四季度
+        /// </summary>
+        [XmlElement("xtQuarter")]
+        public int XtQuarter { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/AIDeposit/RS_DLFDQZLQ.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 组合型智能存款利息明细查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLFDQZLQ : RsBase
+    {
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<MixedTypeAIDepositInterestDetail> List { get; set; }
+    }
+    /// <summary>
+    /// 组合型智能存款利息明细
+    /// </summary>
+    public class MixedTypeAIDepositInterestDetail
+    {
+        /// <summary>
+        /// 起存日期char(8)，格式YYYYMMDD
+        /// </summary>
+        [XmlIgnore]
+        public DateTime StartDate { get; set; }
+        /// <summary>
+        /// 起存日期char(8)，格式YYYYMMDD, 对应<see cref="StartDate"/>
+        /// </summary>
+        [XmlElement("startDate")]
+        public string StartDateStr
+        {
+            get
+            {
+                return this.StartDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.StartDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 支取日期char(8)，格式YYYYMMDD
+        /// </summary>
+        [XmlIgnore]
+        public DateTime DrawDate { get; set; }
+        /// <summary>
+        /// 支取日期char(8)，格式YYYYMMDD, 对应<see cref="DrawDate"/>
+        /// </summary>
+        [XmlElement("drawDate")]
+        public string DrawDateStr
+        {
+            get
+            {
+                return this.DrawDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.DrawDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 支取金额decimal(15,2)
+        /// </summary>
+        [XmlElement("drawAmt")]
+        public decimal DrawAmt { get; set; }
+        /// <summary>
+        /// 实存天varchar(254)
+        /// </summary>
+        [XmlElement("inDays")]
+        public string InDays { get; set; }
+        /// <summary>
+        /// 匹配储种 char(3) D01：1天通知；D07：7天通知；M03：3个月定期；M06：6个月定期；Y01：12个月定期；000：储种为空
+        /// </summary>
+        [XmlElement("mtPerd")]
+        public string MtPerd { get; set; }
+        /// <summary>
+        /// 匹配利率 decimal(9,7)
+        /// </summary>
+        [XmlElement("mtRate")]
+        public decimal MtRate { get; set; }
+        /// <summary>
+        /// 利息decimal(15,2)
+        /// </summary>
+        [XmlElement("inrt")]
+        public decimal Inrt { get; set; }
+        /// <summary>
+        /// 利息状态char(1) 0：未收；1：已收
+        /// </summary>
+        [XmlElement("inrtStatus")]
+        public int InrtStatus { get; set; }
+        /// <summary>
+        /// 收息日期char(8)，格式YYYYMMDD
+        /// </summary>
+        [XmlIgnore]
+        public DateTime RcvDate { get; set; }
+        /// <summary>
+        /// 收息日期char(8)，格式YYYYMMDD, 对应<see cref="RcvDate"/>
+        /// </summary>
+        [XmlElement("rcvDate")]
+        public string RcvDateStr
+        {
+            get
+            {
+                return this.RcvDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.RcvDate = tmp;
+                }
+            }
+        }
+    }
+}
