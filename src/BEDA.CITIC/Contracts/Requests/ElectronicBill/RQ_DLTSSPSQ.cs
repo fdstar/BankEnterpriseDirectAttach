f08e5d38eafcb8281e7fb26426225e6dc01b64e4
fangdong <fangdong@yodoo.net.cn>
2019-07-24 10:27:43 +0800

    中信银行（集团客户电子票据）电子票据 3.1.7 ~ 3.1.8

---
+++ src/BEDA.CITIC/Contracts/Requests/ElectronicBill/RQ_DLTSSPSQ.cs
@@ -0,0 +1,66 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 提示收票申请经办请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLTSSPSQ : RqBase<RS_DLTSSPSQ>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLTSSPSQ"; set { } }
+        /// <summary>
+        /// 客户流水号 varchar (20)
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+        /// <summary>
+        /// 签约账号 char(19)
+        /// </summary>
+        [XmlElement("accountNo")]
+        public string AccountNo { get; set; }
+        /// <summary>
+        /// 电子票据序列号 char(30)
+        /// </summary>
+        [XmlElement("billNo")]
+        public string BillNo { get; set; }
+        /// <summary>
+        /// 经办备注 varchar(256)，可空
+        /// </summary>
+        [XmlElement("memo")]
+        public string Memo { get; set; }
+        /// <summary>
+        /// 预约支付标志 0：非预约1：预约 char(1)
+        /// </summary>
+        [XmlElement("preFlg")]
+        public int PreFlg { get; set; }
+        /// <summary>
+        /// 启用联动预约时间
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? AppointmentTime { get; set; }
+        /// <summary>
+        /// 预约支付日期 预约时非空 char(8) 格式YYYYMMDD
+        /// </summary>
+        [XmlElement("preDate")]
+        public string PreDateStr
+        {
+            get
+            {
+                return this.AppointmentTime?.ToString("yyyyMMdd");
+            }
+            set { }
+        }
+    }
+}
