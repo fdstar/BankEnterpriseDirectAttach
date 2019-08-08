10263c583f5a121c6ae7bcc3041e9f5a2ffbae0e
fangdong <fangdong@yodoo.net.cn>
2019-08-08 16:34:29 +0800

    中信银行（集团客户票据库）票据库 3.1.13 ~ 3.1.14

--- src/BEDA.CITIC/Contracts/Responses/BillBank/RS_DLTSDTQY.cs
+++ src/BEDA.CITIC/Contracts/Responses/BillBank/RS_DLTSDTQY.cs
@@ -9,53 +9,33 @@ using System.Xml.Serialization;
 namespace BEDA.CITIC.Contracts.Responses
 {
     /// <summary>
-    /// 票据库经办查询响应内容
+    /// 代保管票据托收维护明细查询响应内容
     /// </summary>
     [XmlRoot("stream")]
-    public class RS_DLBADQJB : RsBase
+    public class RS_DLTSDTQY : RsBase
     {
         /// <summary>
         /// 中信银行集合列表
         /// </summary>
         [XmlArray("list")]
         [XmlArrayItem("row")]
-        public List<BillOperatingInfo> List { get; set; }
+        public List<AgentBillOperatedInfo> List { get; set; }
     }
     /// <summary>
-    /// 票据库经办
+    /// 代保管经办操作
     /// </summary>
-    public class BillOperatingInfo
+    public class AgentBillOperatedInfo
     {
         /// <summary>
-        /// 代保管账号char(19)
+        /// 批次号varchar(8)
         /// </summary>
-        [XmlElement("accountNo")]
-        public string AccountNo { get; set; }
-        /// <summary>
-        /// 代保管客户名称varchar(122)
-        /// </summary>
-        [XmlElement("cstNm")]
-        public string CstNm { get; set; }
-        /// <summary>
-        /// 代保管机构号varchar(20)
-        /// </summary>
-        [XmlElement("agentBranchNo")]
-        public string AgentBranchNo { get; set; }
-        /// <summary>
-        /// 代保管机构名称varchar(122)
-        /// </summary>
-        [XmlElement("agentBranchNm")]
-        public string AgentBranchNm { get; set; }
+        [XmlElement("batNo")]
+        public string BatNo { get; set; }
         /// <summary>
         /// 票据号varchar(16)
         /// </summary>
         [XmlElement("billNo")]
         public string BillNo { get; set; }
-        /// <summary>
-        /// 票据ID int(20)
-        /// </summary>
-        [XmlElement("billID")]
-        public string BillID { get; set; }
         /// <summary>
         /// 票据类型char(4)，AC01：银承；AC02：商承
         /// </summary>
@@ -66,11 +46,6 @@ namespace BEDA.CITIC.Contracts.Responses
         /// </summary>
         [XmlElement("billAmt")]
         public decimal BillAmt { get; set; }
-        /// <summary>
-        /// 存票批次号varchar(20)
-        /// </summary>
-        [XmlElement("billBathNo")]
-        public string BillBathNo { get; set; }
         /// <summary>
         /// 出票日期char(8)，格式YYYYMMDD
         /// </summary>
@@ -118,28 +93,117 @@ namespace BEDA.CITIC.Contracts.Responses
             }
         }
         /// <summary>
-        /// 存票日期char(8)，格式YYYYMMDD
+        /// 代保管账号char(19)
+        /// </summary>
+        [XmlElement("accountNo")]
+        public string AccountNo { get; set; }
+        /// <summary>
+        /// 代保管客户名称varchar(122)
+        /// </summary>
+        [XmlElement("cstNm")]
+        public string CstNm { get; set; }
+        /// <summary>
+        /// 代保管机构号varchar(20)
+        /// </summary>
+        [XmlElement("agentBranchNo")]
+        public string AgentBranchNo { get; set; }
+        /// <summary>
+        /// 代保管机构名称varchar(122)
+        /// </summary>
+        [XmlElement("agentBranchNm")]
+        public string AgentBranchNm { get; set; }
+        /// <summary>
+        /// 交易状态varchar(2)，值域见附录4.1
+        /// </summary>
+        [XmlElement("stt")]
+        public string Stt { get; set; }
+        /// <summary>
+        /// 银行备注varchar(256)
+        /// </summary>
+        [XmlElement("bankMemo")]
+        public string BankMemo { get; set; }
+        /// <summary>
+        /// 客户备注varchar(512)
+        /// </summary>
+        [XmlElement("cstMemo")]
+        public string CstMemo { get; set; }
+        /// <summary>
+        /// 存票批次号varchar(20)
+        /// </summary>
+        [XmlElement("billBathNo")]
+        public string BillBathNo { get; set; }
+        /// <summary>
+        /// 预约标志char(1)，0：非预约；1：预约
+        /// </summary>
+        [XmlElement("preFlg")]
+        public int PreFlg { get; set; }
+        /// <summary>
+        /// 预约日期char(8)，格式YYYYMMDD
+        /// </summary>
+        [XmlElement("preDate")]
+        public string PreDate { get; set; }
+        /// <summary>
+        /// 预约时间 由<see cref="PreDate"/>组成
         /// </summary>
         [XmlIgnore]
-        public DateTime StoreDate { get; set; }
-        /// <summary>
-        /// 存票日期char(8)，格式YYYYMMDD, 对应<see cref="StoreDate"/>
-        /// </summary>
-        [XmlElement("storeDate")]
-        public string StoreDateStr
+        public DateTime? AppointmentTime
         {
             get
             {
-                return this.StoreDate.ToString("yyyyMMdd");
-            }
-            set
-            {
-                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                if (DateTime.TryParseExact(this.PreDate,
+                   new string[] { "yyyyMMdd" }, CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
                 {
-                    this.StoreDate = tmp;
+                    return tmp;
                 }
+                return null;
             }
         }
+        /// <summary>
+        /// 经办人姓名varchar(62)
+        /// </summary>
+        [XmlElement("operatorNm")]
+        public string OperatorNm { get; set; }
+        /// <summary>
+        /// 经办日期char(8) 格式：YYYYMMDD
+        /// </summary>
+        [XmlElement("operateDate")]
+        public string OperateDate { get; set; }
+        /// <summary>
+        /// 经办时间 char(6)  格式：hhmmss
+        /// </summary>
+        [XmlElement("operateTime")]
+        public string OperateTime { get; set; }
+        /// <summary>
+        /// 经办时间 由<see cref="OperateDate"/>和<see cref="OperateTime"/>组成
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? OperatingTime
+        {
+            get
+            {
+                if (DateTime.TryParseExact(string.Format("{0}{1}", this.OperateDate, this.OperateTime),
+                   new string[] { "yyyyMMdd", "yyyyMMddHHmmss" }, CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    return tmp;
+                }
+                return null;
+            }
+        }
+        /// <summary>
+        /// 付款人开户行行号varchar(20)
+        /// </summary>
+        [XmlElement("payBankNo")]
+        public string PayBankNo { get; set; }
+        /// <summary>
+        /// 付款人开户行名称varchar(122)
+        /// </summary>
+        [XmlElement("payBankNm")]
+        public string PayBankNm { get; set; }
+        /// <summary>
+        /// 付款人开户行地址varchar(60)
+        /// </summary>
+        [XmlElement("payBankAddress")]
+        public string PayBankAddress { get; set; }
         /// <summary>
         /// 付款人账号varchar(30)
         /// </summary>
@@ -151,27 +215,12 @@ namespace BEDA.CITIC.Contracts.Responses
         [XmlElement("payAccNm")]
         public string PayAccNm { get; set; }
         /// <summary>
-        /// 付款人开户行名称varchar(122)
-        /// </summary>
-        [XmlElement("payBankNm")]
-        public string PayBankNm { get; set; }
-        /// <summary>
-        /// 付款人开户行行号varchar(20)
-        /// </summary>
-        [XmlElement("payBankNo")]
-        public string PayBankNo { get; set; }
-        /// <summary>
-        /// 付款人开户行地址varchar(60)
-        /// </summary>
-        [XmlElement("payBankAddress")]
-        public string PayBankAddress { get; set; }
-        /// <summary>
         /// 出票人名称varchar(122)
         /// </summary>
         [XmlElement("drwrName")]
         public string DrwrName { get; set; }
         /// <summary>
-        /// 出票人账号varchar(30)
+        /// 出票人账号varchar(30)，可空
         /// </summary>
         [XmlElement("drwrAccNo")]
         public string DrwrAccNo { get; set; }
@@ -181,7 +230,7 @@ namespace BEDA.CITIC.Contracts.Responses
         [XmlElement("pyeeName")]
         public string PyeeName { get; set; }
         /// <summary>
-        /// 收款人账号varchar(30)
+        /// 收款人账号varchar(30)，可空
         /// </summary>
         [XmlElement("pyeeAccNo")]
         public string PyeeAccNo { get; set; }
@@ -191,25 +240,20 @@ namespace BEDA.CITIC.Contracts.Responses
         [XmlElement("pyeeBankNm")]
         public string PyeeBankNm { get; set; }
         /// <summary>
-        /// 是否曾质押标志char(1)，0：否；1：是
-        /// </summary>
-        [XmlElement("hasImpnFlg")]
-        public int HasImpnFlg { get; set; }
-        /// <summary>
         /// 委托前手背书人名称varchar(122)
         /// </summary>
         [XmlElement("preTendNm")]
         public string PreTendNm { get; set; }
         /// <summary>
-        /// 委托人是否已背书标志char(1)，格式0：否；1：是
-        /// </summary>
-        [XmlElement("hasTendFlg")]
-        public int HasTendFlg { get; set; }
-        /// <summary>
         /// 已背书手数int(3)
         /// </summary>
         [XmlElement("endrAmount")]
         public int EndrAmount { get; set; }
+        /// <summary>
+        /// 是否曾质押标志char(1)，0：否；1：是
+        /// </summary>
+        [XmlElement("hasImpnFlg")]
+        public int HasImpnFlg { get; set; }
         /// <summary>
         /// 是否委托银行收款标志char(1)，0：否；1：是
         /// </summary>
@@ -221,19 +265,9 @@ namespace BEDA.CITIC.Contracts.Responses
         [XmlElement("retmAccNo")]
         public string RetmAccNo { get; set; }
         /// <summary>
-        /// 代保管状态char(2)，值域见附录4.5
+        /// 委托人是否已背书标志char(1)，格式0：否；1：是
         /// </summary>
-        [XmlElement("agentStt")]
-        public string AgentStt { get; set; }
-        /// <summary>
-        /// 银行备注varchar(256)
-        /// </summary>
-        [XmlElement("bankMemo")]
-        public string BankMemo { get; set; }
-        /// <summary>
-        /// 客户备注varchar(512)
-        /// </summary>
-        [XmlElement("cstMemo")]
-        public string CstMemo { get; set; }
+        [XmlElement("hasTendFlg")]
+        public int HasTendFlg { get; set; }
     }
 }
---
+++ src/BEDA.CITIC/Contracts/Responses/BillBank/RS_DLWTDEQY.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 委托大额查询查复明细查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLWTDEQY : RsBase
+    {
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<AgentBillInfo> List { get; set; }
+    }
+}
