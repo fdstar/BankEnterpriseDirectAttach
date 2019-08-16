31ce0acaabf9c17c47e014a72440d538a18ba1b2
fangdong <fangdong@yodoo.net.cn>
2019-08-16 17:01:15 +0800

    中信银行（电子商务）电子商务网上支付 3.1.2

---
+++ src/BEDA.CITIC/Contracts/Requests/ElectronicCommerce/RQ_DL3RDFLE.cs
@@ -0,0 +1,57 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 对账文件下载(第三方支付)请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DL3RDFLE : RqBase<RS_DL3RDFLE>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DL3RDFLE"; set { } }
+        /// <summary>
+        /// 订单日期 格式YYYYMMDD
+        /// </summary>
+        [XmlIgnore]
+        public DateTime QryOrderDate { get; set; }
+        /// <summary>
+        /// 订单日期 格式YYYYMMDD, 对应<see cref="QryOrderDate"/>
+        /// </summary>
+        [XmlElement("qryOrderDate")]
+        public string QryOrderDateStr
+        {
+            get
+            {
+                return this.QryOrderDate.ToString("yyyyMMdd");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.QryOrderDate = tmp;
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// 对账文件下载(直联商户)请求内容
+    /// </summary>
+    public class RQ_DLB2CFLE : RQ_DL3RDFLE
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        public override string Action { get => "DLB2CFLE"; set { } }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/ElectronicCommerce/RS_DL3RDFLE.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 对账文件下载响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DL3RDFLE : RsBase
+    {
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<ReconciliationFile> List { get; set; }
+    }
+    /// <summary>
+    /// 对账文件
+    /// </summary>
+    public class ReconciliationFile
+    {
+        /// <summary>
+        /// 0：充值退回；1：充值；2：提现
+        /// </summary>
+        [XmlElement("type")]
+        public int Type { get; set; }
+        /// <summary>
+        /// 总笔数 int
+        /// </summary>
+        [XmlElement("totalNumber")]
+        public int TotalNumber { get; set; }
+        /// <summary>
+        /// 总金额 deciam(15,2）
+        /// </summary>
+        [XmlElement("totalAmt")]
+        public decimal TotalAmt { get; set; }
+        /// <summary>
+        /// 文件名
+        /// </summary>
+        [XmlElement("fileNm")]
+        public string FileNm { get; set; }
+        /// <summary>
+        /// 对账文件内容 须用base64方式进行解码
+        /// </summary>
+        [XmlElement("fileContent")]
+        public FileContent FileContent { get; set; }
+    }
+}
