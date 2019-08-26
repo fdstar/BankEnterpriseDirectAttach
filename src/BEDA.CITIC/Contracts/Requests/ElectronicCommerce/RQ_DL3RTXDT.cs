3ad102a1c3319ad834b3a8837fd33828e1decf89
fangdong <fangdong@yodoo.net.cn>
2019-08-26 14:25:00 +0800

    中信银行（电子商务）电子商务网上支付 3.1.9 ~ 3.1.10

---
+++ src/BEDA.CITIC/Contracts/Requests/ElectronicCommerce/RQ_DL3RTXDT.cs
@@ -0,0 +1,35 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 第三方商户提现明细查询请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DL3RTXDT : RqBase<RS_DL3RTXDT>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DL3RTXDT"; set { } }
+
+        /// <summary>
+        /// 批次号 varchar(8)，可空 批次号和客户流水号两者必输一项
+        /// </summary>
+        [XmlElement("batNo")]
+        public string BatNo { get; set; }
+        /// <summary>
+        /// 客户流水号 varchar(20)，可空 批次号和客户流水号两者必输一项
+        /// </summary>
+        [XmlElement("clientID")]
+        public string ClientID { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/ElectronicCommerce/RS_DL3RTXDT.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 第三方商户提现明细查询响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DL3RTXDT : RsBase
+    {
+        /// <summary>
+        /// 批次号 varchar(8)
+        /// </summary>
+        [XmlElement("batNo")]
+        public string BatNo { get; set; }
+        
+        /// <summary>
+        /// 中信银行集合列表
+        /// </summary>
+        [XmlArray("list")]
+        [XmlArrayItem("row")]
+        public List<ThirdPartyRechargeCashDetail> List { get; set; }
+    }
+    /// <summary>
+    /// 第三方商户提现明细
+    /// </summary>
+    public class ThirdPartyRechargeCashDetail
+    {
+        /// <summary>
+        /// 提现日期char(8) 格式YYYYMMDD
+        /// </summary>
+        [XmlElement("tranDate")]
+        public string TranDate { get; set; }
+        /// <summary>
+        /// 提现时间char(6) 格式hhmmss
+        /// </summary>
+        [XmlElement("tranTime")]
+        public string TranTime { get; set; }
+        /// <summary>
+        /// 提现时间 由<see cref="TranDate"/>和<see cref="TranTime"/>组成
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? TransactionTime
+        {
+            get
+            {
+                if (DateTime.TryParseExact(string.Format("{0}{1}", this.TranDate, this.TranTime),
+                   new string[] { "yyyyMMdd", "yyyyMMddHHmmss" }, CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    return tmp;
+                }
+                return null;
+            }
+        }
+        /// <summary>
+        /// 商户提现流水号 varchar(20) 
+        /// </summary>
+        [XmlElement("cashFlwNo")]
+        public string CashFlwNo { get; set; }
+        /// <summary>
+        /// 交易金额 decimal(15,2) 
+        /// </summary>
+        [XmlElement("tranAmt")]
+        public decimal TranAmt { get; set; }
+        /// <summary>
+        /// 收款账号 varchar(19)
+        /// </summary>
+        [XmlElement("rcvAccNo")]
+        public string RcvAccNo { get; set; }
+        /// <summary>
+        /// 收款账户名称 varchar(122)
+        /// </summary>
+        [XmlElement("rcvAccNm")]
+        public string RcvAccNm { get; set; }
+        /// <summary>
+        /// 摘要 varchar(102)
+        /// </summary>
+        [XmlElement("abstract")]
+        public string Abstract { get; set; }
+        /// <summary>
+        /// 备注 varchar(60)
+        /// </summary>
+        [XmlElement("memo")]
+        public string Memo { get; set; }
+        /// <summary>
+        /// 交易状态 varchar(2) 值域见附录4.3
+        /// </summary>
+        [XmlElement("stt")]
+        public string Stt { get; set; }
+        /// <summary>
+        /// 交易失败状态char(7)
+        /// </summary>
+        [XmlElement("status")]
+        public string Status { get; set; }
+        /// <summary>
+        /// 交易失败状态信息 varchar(254)
+        /// </summary>
+        [XmlElement("statusText")]
+        public string StatusText { get; set; }
+    }
+}
