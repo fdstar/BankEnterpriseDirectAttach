bd6efebf58897d6407df119fd04243197e847d93
fangdong <fangdong@yodoo.net.cn>
2019-10-31 10:22:29 +0800

    中信银行（供应链金融）核心企业 3.1.5.2

---
+++ src/BEDA.CITIC/Contracts/Requests/CoreEnterprises/RQ_DLCLPCSY.cs
@@ -0,0 +1,64 @@
+﻿using BEDA.CITIC.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Requests
+{
+    /// <summary>
+    /// 日终借款企业额度同步请求内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RQ_DLCLPCSY : RqBase<RS_DLCLPCSY>
+    {
+        /// <summary>
+        /// 业务对应请求代码
+        /// </summary>
+        [XmlElement("action")]
+        public override string Action { get => "DLCLPCSY"; set { } }
+
+        /// <summary>
+        /// 文件名称varchar(30), 额度信息：CREDIT
+        /// </summary>
+        [XmlElement("fileName")]
+        public string FileName { get; set; }
+        /// <summary>
+        /// 文件序号 int (2)
+        /// </summary>
+        [XmlElement("fileSeq")]
+        public int FileSeq { get; set; }
+        /// <summary>
+        /// 文件日期 char(8)格式YYYYMMDD ，可空
+        /// </summary>
+        [XmlIgnore]
+        public DateTime? FileDate { get; set; }
+        /// <summary>
+        /// 文件日期 char(8)格式YYYYMMDD ，可空, 对应<see cref="FileDate"/>
+        /// </summary>
+        [XmlElement("fileDate")]
+        public string FileDateStr
+        {
+            get
+            {
+                return this.FileDate?.ToString("yyyyMMdd");
+            }
+            set
+            {
+                this.FileDate = null;
+                if (DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime tmp))
+                {
+                    this.FileDate = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// 属性1 varchar(100) ，可空,按约定上送
+        /// </summary>
+        [XmlElement("field1")]
+        public string Field1 { get; set; }
+    }
+}
---
+++ src/BEDA.CITIC/Contracts/Responses/CoreEnterprises/RS_DLCLPCSY.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BEDA.CITIC.Contracts.Responses
+{
+    /// <summary>
+    /// 日终借款企业额度同步响应内容
+    /// </summary>
+    [XmlRoot("stream")]
+    public class RS_DLCLPCSY : RsBase
+    {
+        /// <summary>
+        /// 文件名称 varchar(30) , 返回SCF端核心企业ECIF客户号+“_”+CREDIT拼装的文件名称，例：600001614307_CREDIT
+        /// </summary>
+        [XmlElement("fileName")]
+        public string FileName { get; set; }
+        /// <summary>
+        /// 文件大小 int, 单位：字节
+        /// </summary>
+        [XmlElement("fileSize")]
+        public int FileSize { get; set; }
+        /// <summary>
+        /// 数据条数 int
+        /// </summary>
+        [XmlElement("dataCount")]
+        public int DataCount { get; set; }
+        /// <summary>
+        /// 数据内容 varchar(40000)
+        /// </summary>
+        [XmlElement("context")]
+        public string Context { get; set; }
+        /// <summary>
+        /// 文件序号 int
+        /// </summary>
+        [XmlElement("fileSeq")]
+        public int FileSeq { get; set; }
+        /// <summary>
+        /// 文件总数 int
+        /// </summary>
+        [XmlElement("fileCount")]
+        public int FileCount { get; set; }
+        /// <summary>
+        /// 备用字段2 varchar(30) ，按约定返回
+        /// </summary>
+        [XmlElement("field2")]
+        public string Field2 { get; set; }
+        /// <summary>
+        /// 备用字段3 varchar(60) ，按约定返回
+        /// </summary>
+        [XmlElement("field3")]
+        public string Field3 { get; set; }
+        /// <summary>
+        /// 备用字段4 varchar(100) ，按约定返回
+        /// </summary>
+        [XmlElement("field4")]
+        public string Field4 { get; set; }
+    }
+}
